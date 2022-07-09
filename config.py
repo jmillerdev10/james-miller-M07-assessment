5fe52d91714146cc0985d7aa81cd88fd6479d903
James Miller <jammiller.mba@gmail.com>
2022-07-09 15:44:38 -0400

    saving progress

---
+++ config.py
@@ -0,0 +1,13 @@
+# Config file for james-miller-M07-assessment.py
+
+# holidays.json file location
+holidaysjson = "holidays.json"
+
+# strings for the Open Weather Map api
+weatherurl = "https://community-open-weather-map.p.rapidapi.com/forecast/daily"
+querystring = {"q":"rochester,us","lat":"35","lon":"139","cnt":"10","units":"metric or imperial"}
+headers = {
+	        "X-RapidAPI-Key": "a962764033msh54314a0e77932fap1fd034jsnf9488a3be956",
+	        "X-RapidAPI-Host": "community-open-weather-map.p.rapidapi.com"
+            }
+
--- james-miller-M07-assessment.py
+++ james-miller-M07-assessment.py
@@ -4,39 +4,50 @@ from attr import attrs
 from bs4 import BeautifulSoup
 import requests
 from dataclasses import dataclass
+from config import holidaysjson
+from config import weatherurl
+from config import querystring
+from config import headers
 
 holidays_dict = {}
 
 
 # # -------------------------------------------
 # # Modify the holiday class to 
-# # 1. Only accept Datetime objects for date.
+# # 1. Only accept Datetime objects for date. 
 # # 2. You may need to add additional functions
 # # 3. You may drop the init if you are using @dataclasses
 # # --------------------------------------------
-# class Holiday:
-      
-#     def __init__(self,name, date):
-#         #Your Code Here        
+@dataclass
+class Holiday:
+    name: str
+    date: datetime       
     
-#     def __str__ (self):
-#         # String output
-#         # Holiday output when printed.
+    def __str__ (self):
+        # String output
+        # Holiday output when printed.
+        return self.name + " (" + self.date.strftime("%Y-%m-%d") + ")"
           
            
-# # -------------------------------------------
-# # The HolidayList class acts as a wrapper and container
-# # For the list of holidays
-# # Each method has pseudo-code instructions
-# # --------------------------------------------
-# class HolidayList:
-#    def __init__(self):
-#        self.innerHolidays = []
+# -------------------------------------------
+# The HolidayList class acts as a wrapper and container
+# For the list of holidays
+# Each method has pseudo-code instructions
+# --------------------------------------------
+@dataclass
+class HolidayList:
+    innerHolidays: list
+
    
-#     def addHoliday(holidayObj):
-#         # Make sure holidayObj is an Holiday Object by checking the type
-#         # Use innerHolidays.append(holidayObj) to add holiday
-#         # print to the user that you added a holiday
+    def addHoliday(holidayObj):
+        # Make sure holidayObj is an Holiday Object by checking the type
+        # Use innerHolidays.append(holidayObj) to add holiday
+        # print to the user that you added a holiday
+        print("addHoliday UNDER CONSTRUCTION")
+        print("holidayObj type is:")
+        print(type(holidayObj))
+        # HolidayList(innerHolidays).append(holidayObj) # not sure why it is not recognizing innerHolidays. Also tried HolidayList.innerHolidays, but that doesnt work either
+        # print(innerHolidays)
 
 #     def findHoliday(HolidayName, Date):
 #         # Find Holiday in innerHolidays
@@ -47,57 +58,72 @@ holidays_dict = {}
 #         # remove the Holiday from innerHolidays
 #         # inform user you deleted the holiday
 
-#     def read_json(filelocation):
-#         # Read in things from json file location
-#         # Use addHoliday function to add holidays to inner list.
+    def read_json(filelocation):
+        # Read in things from json file location
+        # Use addHoliday function to add holidays to inner list.
+
+        file = open(filelocation)
+        file_dict = json.load(file)
+        print(type(file_dict))
+        print(file_dict["holidays"])
+        for holy_date in file_dict["holidays"]:
+            print("holy_date is: ")
+            print(holy_date)
+            holy_date_name = holy_date["name"]
+            holy_date_date = holy_date["date"]
+            holyobj = Holiday(holy_date_name, holy_date_date)
+            HolidayList.addHoliday(holyobj)
+        return file_dict
 
 #     def save_to_json(filelocation):
 #         # Write out json file to selected file.
         
-def scrapeHolidays(year):
-    # Scrape Holidays from https://www.timeanddate.com/holidays/us/ 
-    # Remember, 2 previous years, current year, and 2  years into the future. You can scrape multiple years by adding year to the timeanddate URL. For example https://www.timeanddate.com/holidays/us/2022
-    # Check to see if name and date of holiday is in innerHolidays array
-    # Add non-duplicates to innerHolidays
-    # Handle any exceptions. 
+    def scrapeHolidays(year):
+        # Scrape Holidays from https://www.timeanddate.com/holidays/us/ 
+        # Remember, 2 previous years, current year, and 2  years into the future. You can scrape multiple years by adding year to the timeanddate URL. For example https://www.timeanddate.com/holidays/us/2022
+        # Check to see if name and date of holiday is in innerHolidays array
+        # Add non-duplicates to innerHolidays
+        # Handle any exceptions. 
+        print("UNDER CONSTRUCTION")
+        
 
-    def getHTML(url):
-        response = requests.get(url)
-        return response.text
+        def getHTML(url):
+            response = requests.get(url)
+            return response.text
 
-    # year = 2022
-    # print("https://www.timeanddate.com/holidays/us/%s?hol=33554809" % (year))
-    # html = getHTML("https://www.timeanddate.com/holidays/us/%s?hol=33554809" % (year))
-    html = getHTML("https://www.timeanddate.com/holidays/us/2022?hol=33554809")
-    soup = BeautifulSoup(html,'html.parser')
+        # year = 2022
+        # print("https://www.timeanddate.com/holidays/us/%s?hol=33554809" % (year))
+        # html = getHTML("https://www.timeanddate.com/holidays/us/%s?hol=33554809" % (year))
+        html = getHTML("https://www.timeanddate.com/holidays/us/2022?hol=33554809")
+        soup = BeautifulSoup(html,'html.parser')
 
-    table = soup.find('tbody').find_all('tr')
-    # print(list(table))
-    holidays = []
+        table = soup.find('tbody').find_all('tr')
+        # print(list(table))
+        holidays = []
 
-    for row in table:
-        holiday = {}
-        # title = row.find('img')
-        if row.find('th') is not None:
-            # print(row.find('th').text)
-            holiday['date'] = row.find('th').text
-        if row.find('a') is not None:
-            # print(row.find('a').text)
-            holiday['name'] = row.find('a').text
-        # print([holiday['date'], holiday['name']])
-        # print(row.find(class_ = 'star-rating')["class"][1])
-        holidays.append(holiday)
+        for row in table:
+            holiday = {}
+            # title = row.find('img')
+            if row.find('a') is not None:
+                # print(row.find('a').text)
+                holiday['name'] = row.find('a').text
+            if row.find('th') is not None:
+                # print(row.find('th').text)
+                holiday['date'] = row.find('th').text
+            # print([holiday['date'], holiday['name']])
+            # print(row.find(class_ = 'star-rating')["class"][1])
+            holidays.append(holiday)
 
-    for holiday in holidays:
-        if holiday == {}:
-            holidays.remove(holiday)
-    # jholidays = {'%s holidays' % year: holidays}
+        for holiday in holidays:
+            if holiday == {}:
+                holidays.remove(holiday)
+        # jholidays = {'%s holidays' % year: holidays}
 
-    # test_file = open('json_file.json', 'w')
-    # json.dump(jholidays, test_file, indent = 4)
-    # test_file.close()
-    # print(list(holidays))
-    return holidays
+        # test_file = open('json_file.json', 'w')
+        # json.dump(jholidays, test_file, indent = 4)
+        # test_file.close()
+        # print(list(holidays))
+        return holidays
 
 #     def numHolidays():
 #         # Return the total number of holidays in innerHolidays
@@ -113,11 +139,14 @@ def scrapeHolidays(year):
 #         # Output formated holidays in the week. 
 #         # * Remember to use the holiday __str__ method.
 
-#     def getWeather(weekNum):
-#         # Convert weekNum to range between two days
-#         # Use Try / Except to catch problems
-#         # Query API for weather in that week range
-#         # Format weather information and return weather string.
+    def getWeather(weekNum): 
+        # Convert weekNum to range between two days
+        # Use Try / Except to catch problems
+        # Query API for weather in that week range
+        # Format weather information and return weather string.
+        response = requests.request("GET", weatherurl, headers=headers, params=querystring)    
+        print(response.text)
+        
 
 #     def viewCurrentWeek():
 #         # Use the Datetime Module to look up current week and year
@@ -130,6 +159,7 @@ def scrapeHolidays(year):
 
 
 def main():
+    global holidays_dict
     # Large Pseudo Code steps
     # -------------------------------------
     # 1. Initialize HolidayList Object
@@ -140,14 +170,22 @@ def main():
     # 5. Take user input for their action based on Menu and check the user input for errors
     # 6. Run appropriate method from the HolidayList object depending on what the user input is
     # 7. Ask the User if they would like to Continue, if not, end the while loop, ending the program.  If they do wish to continue, keep the program going. 
-    for i in range(2020, 2025):
-        year = i
-        jholidays = scrapeHolidays(year)
-        holidays_dict["%s holidays" % (year)] = jholidays
-    # print(holidays_dict)
-    test_file = open('json_file.json', 'w')
-    json.dump(holidays_dict, test_file, indent = 4)
-    test_file.close()
+    # booty = open(locdog)
+    # butt = json.load(booty)
+    # print(butt)
+    
+    print(holidays_dict)
+    # response = requests.get(apiurl, headers)
+    # response = requests.request("GET", weatherurl, headers=headers, params=querystring)    
+    # print(response.text)
+    # for i in range(2020, 2025):
+    #     year = i
+    #     jholidays = scrapeHolidays(year)
+    #     holidays_dict["%s holidays" % (year)] = jholidays
+    # # print(holidays_dict)
+    # test_file = open('json_file.json', 'w')
+    # json.dump(holidays_dict, test_file, indent = 4)
+    # test_file.close()
 
 if __name__ == "__main__":
     main();
